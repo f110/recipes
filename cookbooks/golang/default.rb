89374cc15c118ffec1af387895ee2022017fc2fd
Fumihiro Ito <fmhrit@gmail.com>
2017-04-02 14:08:23 +0900

    use golang apt repository

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -1,4 +1,11 @@
-package "golang" do; end
+execute "add golang repository" do
+  action :run
+  command "add-apt-repository -y -u ppa:longsleep/golang-backports"
+  user "root"
+  not_if "grep -q \"longsleep/golang-backports\" /etc/apt/sources.list.d -R"
+end
+
+package "golang-go" do; end
 
 directory "#{node[:home]}/local/go" do
   action :create
